13f707c342b23cb524153b096dfdfdfae3a74b37
Michael Lyle <mlyle@lyle.org>
2023-12-18 21:38:09 -0800

    add const qualifier to string in lcd_blit_string

--- src/lcd.c
+++ src/lcd.c
@@ -229,7 +229,7 @@ void lcd_blit_char(uint8_t c, int x, int y, uint8_t r, uint8_t g, uint8_t b,
 	lcd_blit_char_internal(c, x, y, r, g, b, bgr, bgg, bgb);
 }
 
-int lcd_blit_string(char *str, int x, int y, uint8_t r, uint8_t g, uint8_t b,
+int lcd_blit_string(const char *str, int x, int y, uint8_t r, uint8_t g, uint8_t b,
 		uint8_t bgr, uint8_t bgg, uint8_t bgb) {
 	while ((*str) && (x <= 150)) {
 		lcd_blit_char_internal(*str, x, y, r, g, b, bgr, bgg, bgb);
